ab561928fb93af5e54ef86548980d1053b4a5f06
Schultze-Melling <jschultzemelling@kpmg-law.com>
2024-11-19 14:49:01 +0100

    Update to ES modules and fix dependencies

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 // Serve index.html for all routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
